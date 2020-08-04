7029fd9f8de82ea013d16a743b428c170de0f64f
kirbyasdf <kirbyaugustus@gmail.com>
2020-08-03 22:32:50 -0400

    withrouter

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage/Homepage.js";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- clothing-store/src/index.js
+++ clothing-store/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter><App /></BrowserRouter>,
   document.getElementById("root"),
 );
