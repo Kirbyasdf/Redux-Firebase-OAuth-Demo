7e481047aa866069508dd46ae9b6493fd9a44322
kirbyasdf <kirbyaugustus@gmail.com>
2020-08-06 16:48:31 -0400

    redirect for user presence

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/users.actions";
 
@@ -10,8 +10,9 @@ import Header from "./components/header/Header";
 import HomePage from "./pages/homepage/HomePage.js";
 import ShopPage from "./pages/shop/ShopPage.js";
 import SignUpSignIn from "./pages/sign-up-sign-in/SignUpSignIn";
+import SignUp from "./components/sign-up/SignUp";
 
-function App({ setCurrentUser }) {
+function App({ setCurrentUser, currentUser }) {
   useEffect(() => {
     const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -36,14 +37,17 @@ function App({ setCurrentUser }) {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignUpSignIn} />
+        <Route exact path="/signin" render={() => (currentUser ? <Redirect to="/" /> : <SignUpSignIn />)} />
       </Switch>
     </div>
   );
 }
 
+const msp = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 const mdp = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mdp)(App);
+export default connect(msp, mdp)(App);
