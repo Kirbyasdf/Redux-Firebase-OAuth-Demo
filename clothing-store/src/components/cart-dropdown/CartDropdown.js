a4dd1b6ec052b5e777887f28a7371504022a0a57
kirbyasdf <kirbyaugustus@gmail.com>
2020-08-06 22:22:06 -0400

    cart item preview

--- clothing-store/src/components/cart-dropdown/CartDropdown.js
+++ clothing-store/src/components/cart-dropdown/CartDropdown.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/CustomButton";
+import CartItem from "../cart-item/CartItem";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems.map((ci) => (
+        <CartItem key={ci.id} item={ci} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const msp = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(msp, null)(CartDropdown);
